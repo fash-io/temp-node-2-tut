c7a94c0255a71a9e5c147008ca0215478e1485b3
fash-io <135979222+fash-io@users.noreply.github.com>
2023-06-25 05:21:42 +1200

    first commit

---
+++ 8-os-module.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+// info abut current user
+
+const user = os.userInfo()
+console.log(user);
+
+
+//  methos returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+    name:os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+    freeMem:os.freemem()
+}
+
+console.log(currentOS);
